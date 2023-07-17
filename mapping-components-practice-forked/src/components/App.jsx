57a06729d29074369599a7a20115ffdc94b7b631
Dam <110803260+DamMembrive@users.noreply.github.com>
2023-07-17 16:04:37 -0300

    Add files via upload

---
+++ mapping-components-practice-forked/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Entry from "./Entry";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        <Entry
+          emoji="💪"
+          name="Tense Biceps"
+          description="“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+        />
+        <Entry />
+        <Entry />
+      </dl>
+    </div>
+  );
+}
+
+export default App;
